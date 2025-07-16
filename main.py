acba60b0a22a57cebb2634ef86bd3dc6bf8e79ab
FerchoJpt-Controlc <jesustarax6@gmail.com>
2025-07-15 22:57:09 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,26 @@
+inventario={}
+
+cantidad=int(input("Ingrese la cantidad de productos: "))
+
+for i in range(cantidad):
+    print(f"\nProducto #{i+1}")
+
+    while True:
+        codigo=input("Ingrese el codigo del producto: ")
+        if codigo in inventario:
+            print("codigo ya existente")
+        else:
+            break
+
+    nombre=input("Ingrese el nombre del producto: ")
+
+    categoria=input("Ingrese(Hombre,mujer,niño)")
+
+    while True:
+        talla=input("Ingrese la tallea: ")
+        if talla in ["S", "M", "L", "XL"]:
+            break
+        else:
+            print("Talla no existente")
+
+
